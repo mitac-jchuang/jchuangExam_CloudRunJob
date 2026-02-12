690561312d407f74113faec1c70994d1358ca357
jchuang <jchuang@mitac.com.tw>
2026-02-12 10:03:41 +0800

    20260212-1002 testing getenv

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 import sys
-import logging
-import google.cloud.logging
+#import logging
+#import google.cloud.logging
 import time
 
 #client = google.cloud.logging.Client(project=os.getenv("PROJECT_ID"))
@@ -12,6 +12,8 @@ if __name__ == "__main__":
     start_time = time.perf_counter()
     print(f"Test Run start at {start_time}")
     #logging.info(f"Test Run start at {start_time}")
+    
+    print(os.getenv("PROJECT_ID"))
     
     if len(sys.argv) > 1:
         print(f"Received input parameter: {sys.argv[1]}")
