f560f873431c40db1eb7ff1679b0764fcce24f01
jchuang <jchuang@mitac.com.tw>
2026-02-12 14:17:50 +0800

    20260212-1416 modify build process with arguments

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ if __name__ == "__main__":
     #logging.info(f"Test Run start at {start_time}")
     
     print(os.getenv("PROJECT_ID"))
+    print(os.getenv("BRONZE_BUCKET"))
     
     if len(sys.argv) > 1:
         print(f"Received input parameter: {sys.argv[1]}")
